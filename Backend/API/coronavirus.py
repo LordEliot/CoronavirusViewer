1abfead114eff0381b13c80d9a29e3ae51e5ead2
Diego Alejandro Vidales Rubio <diegovidales6@gmail.com>
2020-04-05 18:27:25 -0500

    Casos Colombia-Tolima-Ibague

--- Backend/API/coronavirus.py
+++ Backend/API/coronavirus.py
@@ -4,13 +4,63 @@ import requests
 request = requests.get('https://www.datos.gov.co/resource/gt2j-8ykr.json')
 response = request.json()
 
-print("Coronavirus project: Colombia")
-print("Current cases: " + str(len(response)))
+print("Proyecto Covid-19: Colombia")
+print("------------------------------------------")
+print("Nacional:")
+print("")
+
+counter_recovered = 0
+counter_death = 0
+
+for case in response:
+    if case["atenci_n"] == "Recuperado":
+        counter_recovered += 1
+    if case["atenci_n"] == "Fallecido":
+        counter_death += 1
+
+print("Casos positivos: " + str(len(response)))
+print("Casos recuperados: " + str(counter_recovered))
+print("Muertes: " + str(counter_death))
+
+print("------------------------------------------")
+print("Tolima:")
+print("")
 
 counter = 0
+counter_recovered = 0
+counter_death = 0
+
+for case in response:
+    if case["departamento"] == "Tolima":
+        counter += 1
+    if case["departamento"] == "Tolima" and case["atenci_n"] == "Recuperado":
+        counter_recovered += 1
+    if case["departamento"] == "Tolima" and case["atenci_n"] == "Fallecido":
+        counter_death += 1
+
+print("Casos positivos: " + str(counter))
+print("Casos recuperados: " + str(counter_recovered))
+print("Muertes: " + str(counter_death))
+
+print("------------------------------------------")
+print("Ibague:")
+print("")
+
+counter = 0
+counter_recovered = 0
+counter_death = 0
 
 for case in response:
     if case["ciudad_de_ubicaci_n"] == u"Ibagué":
-        counter = counter + 1
+        counter += 1
+    if case["atenci_n"] == "Recuperado" and case["ciudad_de_ubicaci_n"] == u"Ibagué":
+        counter_recovered += 1
+     if case["ciudad_de_ubicaci_n"] == u"Ibagué" and case["atenci_n"] == "Fallecido":
+        counter_death += 1
 
-print("Cases in Ibague: " + str(counter))
+print("Casos en Ibague: " + str(counter))
+print("Casos recuperados en Ibague: " + str(counter_recovered))
+print("Muertes: " + str(counter_death))
+
+print("-----------------------")
+print("Hecho por Diego Vidales")
